4a9b5296929db6e27df49fda38a42cf76536877a
Bruno Lacerda <brunolcpg@gmail.com>
2020-05-23 17:36:26 -0300

    Adiciona função para buscar cliente por id

--- src/main/java/com/brunopego/api/controller/ClienteController.java
+++ src/main/java/com/brunopego/api/controller/ClienteController.java
@@ -1,9 +1,12 @@
 package com.brunopego.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brunopego.domain.model.Cliente;
@@ -18,6 +21,17 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 }
