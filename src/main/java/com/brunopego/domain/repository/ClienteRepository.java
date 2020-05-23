442b07692642a94c037c581d4b404b7035d84de9
Bruno Lacerda <brunolcpg@gmail.com>
2020-05-23 17:35:46 -0300

    Adiciona função para filtrar cliente por nome

--- src/main/java/com/brunopego/domain/repository/ClienteRepository.java
+++ src/main/java/com/brunopego/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.brunopego.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.brunopego.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNome(String nome);
+	
+	List<Cliente> findByNomeContaining(String nome);
 
 }
