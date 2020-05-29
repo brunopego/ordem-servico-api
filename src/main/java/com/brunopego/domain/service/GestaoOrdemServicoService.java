4424a295c2d9c4dc03da7b1c94417919acd8325e
Bruno Lacerda <brunolcpg@gmail.com>
2020-05-29 18:28:17 -0300

    Adicionar método para adicionar comentário

--- src/main/java/com/brunopego/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/brunopego/domain/service/GestaoOrdemServicoService.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.brunopego.domain.exception.NegocioException;
 import com.brunopego.domain.model.Cliente;
+import com.brunopego.domain.model.Comentario;
 import com.brunopego.domain.model.OrdemServico;
 import com.brunopego.domain.model.StatusOrdemServico;
 import com.brunopego.domain.repository.ClienteRepository;
+import com.brunopego.domain.repository.ComentarioRepository;
 import com.brunopego.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -20,6 +22,9 @@ public class GestaoOrdemServicoService {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ComentarioRepository comentarioRepository;
 
 	public OrdemServico criar(OrdemServico ordemServico) {
 		
@@ -30,6 +35,20 @@ public class GestaoOrdemServicoService {
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 		return ordemServicoRepository.save(ordemServico);
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new NegocioException("Ordem de serviçonão encontrada"));
+		
+		Comentario comentario = new Comentario();
+		comentario.setDataEnvio(OffsetDateTime.now());
+		comentario.setDescricao(descricao);
+		comentario.setOrdemServico(ordemServico);
+		
+		return comentarioRepository.save(comentario);
+		
 	}
 	
 }
