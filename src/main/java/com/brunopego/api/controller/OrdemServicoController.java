1fccf02c08a0e265ccd84961d5c058bca94ea101
Bruno Lacerda <brunolcpg@gmail.com>
2020-05-29 17:16:40 -0300

    Adiciona model/dto e utilização do ModelMapper

--- src/main/java/com/brunopego/api/controller/OrdemServicoController.java
+++ src/main/java/com/brunopego/api/controller/OrdemServicoController.java
@@ -1,16 +1,27 @@
 package com.brunopego.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brunopego.api.model.OrdemServicoModel;
 import com.brunopego.domain.model.OrdemServico;
+import com.brunopego.domain.repository.OrdemServicoRepository;
 import com.brunopego.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -20,10 +31,44 @@ public class OrdemServicoController {
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServico;
 	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
-		return gestaoOrdemServico.criar(ordemServico);
+	@ResponseStatus(HttpStatus.CREATED) 
+	public OrdemServicoModel criar(@Valid @RequestBody OrdemServico ordemServico) {
+		return toModel(gestaoOrdemServico.criar(ordemServico));
+	}
+	
+	@GetMapping
+	public List<OrdemServicoModel> listar() {
+		return toCollectionModel(ordemServicoRepository.findAll());
+	}
+	
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServicoModel> buscar(@PathVariable Long ordemServicoId) {
+		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+		
+		if (ordemServico.isPresent()) {
+			OrdemServicoModel ordemServicoModel = toModel(ordemServico.get());
+			return ResponseEntity.ok(ordemServicoModel);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	private OrdemServicoModel toModel(OrdemServico ordemServico) {
+		return modelMapper.map(ordemServico, OrdemServicoModel.class); 
+	}
+	
+	private List<OrdemServicoModel> toCollectionModel(List<OrdemServico> ordensServico) {
+		return ordensServico.stream()
+				.map(ordemServico -> toModel(ordemServico))
+				.collect(Collectors.toList());
+		
 	}
 
 }
